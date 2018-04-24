bbb398ebb31b5bae96fa57b72febdf87dae178dc
Ubuntu <ubuntu@ip-172-31-46-23.us-west-2.compute.internal>
2018-04-24 23:42:06 +0000

    Modifications to files to allow for streaming Pensieve from a server to external (non-local) clients

--- real_exp/run_exp.py
+++ real_exp/run_exp.py
@@ -6,15 +6,16 @@ import numpy as np
 
 RUN_SCRIPT = 'run_video.py'
 RANDOM_SEED = 42
-RUN_TIME = 280  # sec
-ABR_ALGO = ['fastMPC', 'robustMPC', 'BOLA', 'RL']
-REPEAT_TIME = 10
+RUN_TIME = 60  # sec
+#ABR_ALGO = ['fastMPC', 'robustMPC', 'BOLA', 'RL']
+ABR_ALGO = ['RL']
+REPEAT_TIME = 1
 
 
 def main():
 
 	np.random.seed(RANDOM_SEED)
-
+        print "hi hudson"
 	with open('./chrome_retry_log', 'wb') as log:
 		log.write('chrome retry log\n')
 		log.flush()
@@ -22,20 +23,21 @@ def main():
 		for rt in xrange(REPEAT_TIME):
 			np.random.shuffle(ABR_ALGO)
 			for abr_algo in ABR_ALGO:
-
+                                print "abr_algo is: " + abr_algo
 				while True:
-
+                                        print "running python script"
 					script = 'python ' + RUN_SCRIPT + ' ' + \
 							  abr_algo + ' ' + str(RUN_TIME) + ' ' + str(rt)
-					
 					proc = subprocess.Popen(script,
 							  stdout=subprocess.PIPE, 
 							  stderr=subprocess.PIPE, 
 							  shell=True)
 
+					print "ran run_video.py"
 					(out, err) = proc.communicate()
 
 					if out == 'done\n':
+                                                print "should be finished"
 						break
 					else:
 						log.write(abr_algo + '_' + str(rt) + '\n')
--- real_exp/run_video_modified.py
+++ real_exp/run_video_modified.py
@@ -2,12 +2,12 @@ import os
 import sys
 import signal
 import subprocess
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException
-from pyvirtualdisplay import Display
+#from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.common.action_chains import ActionChains
+#from selenium.webdriver.common.keys import Keys
+#from selenium.common.exceptions import TimeoutException
+#from pyvirtualdisplay import Display
 from time import sleep
 
 # TO RUN: download https://pypi.python.org/packages/source/s/selenium/selenium-2.39.0.tar.gz
@@ -22,8 +22,8 @@ from time import sleep
 # if they are at those locations, don't need to specify
 
 
-def timeout_handler(signum, frame):
-	raise Exception("Timeout")
+#def timeout_handler(signum, frame):
+#	raise Exception("Timeout")
 
 abr_algo = sys.argv[1]
 run_time = int(sys.argv[2])
@@ -34,18 +34,24 @@ exp_id = sys.argv[3]
 # ---------------------------------------------------
 #          |
 #          v
-url = 'http://localhost/' + 'myindex_' + abr_algo + '.html'
+url = 'http://52.24.78.232/' + 'myindex_' + abr_algo + '.html'
 # timeout signal
-signal.signal(signal.SIGALRM, timeout_handler)
-signal.alarm(run_time + 30)
+#signal.signal(signal.SIGALRM, timeout_handler)
+#signal.alarm(run_time + 30)
 	
 try:
+        #Following 4 lines added by hudson to address frequent "ADDR ALREADY IN USE" errors
+        command1 = 'lsof -ti:8333 | xargs kill -9'
+        proc1 = subprocess.Popen(command1, shell=True)
+        sleep(2)
+        proc1.kill()
+
 	# copy over the chrome user dir
 
-	default_chrome_user_dir = '../abr_browser_dir/chrome_data_dir'
-	chrome_user_dir = '/tmp/chrome_user_dir_real_exp_' + abr_algo
-	os.system('rm -r ' + chrome_user_dir)
-	os.system('cp -r ' + default_chrome_user_dir + ' ' + chrome_user_dir)
+#	default_chrome_user_dir = '../abr_browser_dir/chrome_data_dir'
+#	chrome_user_dir = '/tmp/chrome_user_dir_real_exp_' + abr_algo
+#	os.system('rm -r ' + chrome_user_dir)
+#	os.system('cp -r ' + default_chrome_user_dir + ' ' + chrome_user_dir)
 	
 	# start abr algorithm server
 	if abr_algo == 'RL':
@@ -57,48 +63,47 @@ try:
 	else:
 		command = 'exec /usr/bin/python ../rl_server/simple_server.py ' + abr_algo + ' ' + exp_id
 	
-	proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	proc = subprocess.Popen(command, shell=True)
 	sleep(2)
-	
+	#Following 10 lines or so start up a selenium virtual display and play the video in it
 	# to not display the page in browser
-	display = Display(visible=1, size=(800,600))
-	display.start()
+#	display = Display(visible=1, size=(800,600))
+#	display.start()
 	
 	# initialize chrome driver
-	options=Options()
-	chrome_driver = '../abr_browser_dir/chromedriver'
-	options.add_argument('--user-data-dir=' + chrome_user_dir)
-	options.add_argument('--ignore-certificate-errors')
-	driver=webdriver.Chrome(chrome_driver, chrome_options=options)
+#	options=Options()
+#	chrome_driver = '../abr_browser_dir/chromedriver'
+#	options.add_argument('--user-data-dir=' + chrome_user_dir)
+#	options.add_argument('--ignore-certificate-errors')
+#	driver=webdriver.Chrome(chrome_driver, chrome_options=options)
 	
 	# run chrome
-	driver.set_page_load_timeout(10)
-	driver.get(url)
+#	driver.set_page_load_timeout(10)
+#	driver.get(url)
 	
 	sleep(run_time)
 	
-	driver.quit()
-	display.stop()
+#	driver.quit()
+#	display.stop()
 	
 	# kill abr algorithm server
 	proc.send_signal(signal.SIGINT)
-	# proc.kill()
+	#proc.kill()
+
 	
 	print 'done'
 	
 except Exception as e:
-	try: 
-		display.stop()
-	except:
-		pass
+#	try: 
+#		display.stop()
+#	except:
+#		pass
+#	try:
+#		driver.quit()
+#	except:
+#		pass
 	try:
-		driver.quit()
+	        proc.send_signal(signal.SIGINT)
 	except:
 		pass
-	try:
-		proc.send_signal(signal.SIGINT)
-	except:
-		pass
-	
 	print e	
-
