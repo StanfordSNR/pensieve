f4bd026411a9014f4f5da3fed15c9e5fbab48d52
Francis Y. Yan <fyy@cs.stanford.edu>
2020-01-12 18:58:22 -0800

    Don't set MKL_NUM_THREADS (not used by numpy and throw an error)

--- multi_video_sim/rl_test.py
+++ multi_video_sim/rl_test.py
@@ -7,7 +7,6 @@ import socket
 os.environ['CUDA_VISIBLE_DEVICES']=''
 os.environ['OMP_NUM_THREADS'] = '1' # export OMP_NUM_THREADS=1
 os.environ['OPENBLAS_NUM_THREADS'] = '1' # export OPENBLAS_NUM_THREADS=1
-os.environ['MKL_NUM_THREADS'] = '1' # export MKL_NUM_THREADS=1
 os.environ['VECLIB_MAXIMUM_THREADS'] = '1' # export VECLIB_MAXIMUM_THREADS=1
 os.environ['NUMEXPR_NUM_THREADS'] = '1' # export NUMEXPR_NUM_THREADS=1
 import numpy as np
